03fa44a7789b35400e94bbbd5400a8724c2cf3b6
ritusoni2298 <ritusoni2298@gmail.com>
2019-09-13 14:35:01 -0400

    msg

--- Demo1.py
+++ Demo1.py
@@ -4,9 +4,3 @@ b=20
 print("a and b are:",a,b)
 print("Making new changes and adding two values a and b:",a+b)
 z=30
-<<<<<<< HEAD
-z=0
-
-=======
-z=1
->>>>>>> 2ba72c92a9e7015dbf3d18124d6fa7e8e1a49c74
